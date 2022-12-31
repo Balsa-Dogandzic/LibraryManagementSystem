a08580471e669a3b8603f2fdcdfade2e893d8be1
Dzoganovic_Stefan <dzoganovicstefan@gmail.com>
2022-12-31 15:20:53 +0100

    Made a new Book

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,8 @@ public class Test {
 	
 	public void test() {
 		Book b = new Book("Knjiga o dzungli", 200, "Kipling");
+		Book b1 = new Book("Stefan i ja", 300, "Dzoganovic Stefan");
 		System.out.println(b.toString());
+		System.out.println(b1.toString());
 	}
 }
