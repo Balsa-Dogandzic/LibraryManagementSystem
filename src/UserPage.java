79de52215892961997f70466c33cd5d84ea8e359
Dzoganovic_Stefan <dzoganovicstefan@gmail.com>
2023-01-08 09:42:18 +0100

    Started working on book_reservation

--- src/UserPage.java
+++ src/UserPage.java
@@ -4,12 +4,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JTable;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Dimension;
 
 public class UserPage extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel panel;
+	private JPanel panel_1;
+	private JLabel lblNewLabel;
+	private JPanel panel_2;
+	private JPanel panel_3;
 	private JTable table;
+	private JTable table_1;
 
 	/**
 	 * Launch the application.
@@ -37,12 +49,40 @@ public class UserPage extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
-		
-		table = new JTable();
-		panel.add(table);
+
+		panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setHgap(20);
+		flowLayout.setVgap(20);
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		contentPane.add(panel, BorderLayout.NORTH);
+
+		lblNewLabel = new JLabel("Welcome:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		panel.add(lblNewLabel);
+
+		panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+
+		panel_2 = new JPanel();
+		panel_1.add(panel_2);
+
+		panel_3 = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) panel_3.getLayout();
+		flowLayout_1.setVgap(60);
+		flowLayout_1.setHgap(0);
+		panel_1.add(panel_3);
+
+		DefaultTableModel model = new DefaultTableModel();
+		model.addRow(new Object[] {});
+		JTable table = new JTable(
+				new DefaultTableModel(new Object[][] {}, new String[] { "ISBN", "Reservation Date", "Return Date" }));
+		table.setPreferredSize(new Dimension(247, 231));
+		table.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		table.setMinimumSize(new Dimension(247, 231));
+		panel_3.add(table);
+
 	}
 
 }
